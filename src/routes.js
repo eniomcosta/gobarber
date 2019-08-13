0625a0dd348a663d918cac605d4bca39193ddd6e
Enio Costa <enio.m.costa@gmail.com>
2019-08-13 19:25:22 -0300

    Created Appointment entity, insert route. Removed 'name' property on Appointment migration.

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,6 +16,7 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+/** Authentication middleware */
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
@@ -22,5 +24,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/file', upload.single('file'), FileController.store);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
